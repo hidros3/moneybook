8e7c2f9721c8ea5d0f8ff6dd3ba2fa9587ce71e6
taekjoo <taekjoo@gmail.com>
2015-07-23 12:36:34 +0900

    almost done

--- refactoring.rb
+++ refactoring.rb
@@ -3,19 +3,13 @@ require 'base64'
 require 'json'
 require 'pg'
 require 'sequel'
-
-# # Initialize the API
-
-
-# # Show the user's labels
-# results = @client.execute!(
-#   :api_method => @gmail_api.users.messages.list,
-#   :parameters => { :userId => 'me', :q => 'from:dailyreport@samsungcard.com' })
+require 'nokogiri'
 
 # # READ TABLE FROM DB
 # DB = Sequel.connect("postgres://localhost/moneybookdb")
 # @mails = DB[:mails]
 # @spends = DB[:spends]
+
 
 def authorize
   FileUtils.mkdir_p(File.dirname(CREDENTIALS_PATH))
@@ -38,55 +32,70 @@ end
 
 class MoneyBook < Google::APIClient
 
-
   def initialize
     @client = Google::APIClient.new(:application_name => APPLICATION_NAME)
     @client.authorization = authorize
     @gmail_api = @client.discovered_api('gmail', 'v1')
+    @DB = Sequel.connect("postgres://localhost/moneybookdb")
+    @mails = @DB[:mails]
+    @spends = @DB[:spends]
+
   end
 
-  def gets_lists
+  def gets
     @lists = @client.execute!(
     :api_method => @gmail_api.users.messages.list,
-    :parameters => { :userId => 'taekjoo@gmail.com', :q => 'from:dailyreport@samsungcard.com' })
-  end
-
-  def gets_details
+    :parameters => { :userId => 'me', :q => 'from:dailyreport@samsungcard.com' })
+    
+    @html = []
+    
     @lists.data.messages.each do |list|
-      message = @client.execute!(
-      :api_method => @gmail_api.users.messages.get,
-      :parameters => { :userId => 'taekjoo@gmail.com', :id => '14eb445940f4236d' })
-    p message
-    break
+      begin
+        unless @mails.where( :mail_id => list.id ).any?
+          @mails.insert( :mail_id => list.id )
+          @message = @client.execute!(
+          :api_method => @gmail_api.users.messages.get,
+          :parameters => { :userId => 'me', :id => list.id })
+          
+          if JSON.parse(@message.data.to_json)["payload"]["body"]["data"]
+            @encoded_message = Base64.urlsafe_decode64(JSON.parse(@message.data.to_json)["payload"]["body"]["data"])
+          end
+
+          @html << @encoded_message
+
+        end
+      rescue Exception => e
+        puts "exception"
+      end
+    end    
+    
+    @html.each do |h|
+      doc = Nokogiri::HTML::Document.parse(h.to_s, nil, "UTF-8")
+      
+      data = doc.css('body table td tr table tr').map(&:text)
+      
+      data.each do |d|
+        row = d.gsub(/[[:blank:]]/,'').gsub(",","").gsub("\r\n", ',').gsub(/\A,|,\Z/,'').split(',')
+        if row.grep(/\A\d\d-\d\d/).any? 
+          p row 
+          @spends.insert( :date => row[0],
+                          :time => row[1],
+                          :type => row[2],
+                          :user => row[3],
+                          :card_name => row[4],
+                          :card_num => row[5],
+                          :store_name => row[6],
+                          :price => row[7],
+                          :installation => row[8] ,
+                          :success => row[9]
+                          )
+        end
+      end
     end
   end
 
 
-
-
-
-
-  # def samsung_card
-  #   a = []
-  #   mails = DB[:mails]
-  #   @lists.data.messages.each do |list|
-  #     unless mails.where(:mail_id => "#{list.id}").any?
-  #       mails.insert(:mail_id => "#{list.id}")
-  #       messages = @client.execute!(
-  #         :api_method => @gmail_api.users.messages.get,
-  #         :parameters => { :userId => 'me', :id => "#{list.id}"})
-  #       if JSON.parse(messages.data.to_json)["payload"]["body"]["data"]
-  #         encoded_message = Base64.urlsafe_decode64(JSON.parse(messages.data.to_json)["payload"]["body"]["data"])
-  #         a << encoded_message
-  #       end
-  #     end
-  #   end
-  #   mails.save
-  #   return a
-  # end
-
 end
 
 m = MoneyBook.new
-m.gets_lists
-m.gets_details
+m.gets
